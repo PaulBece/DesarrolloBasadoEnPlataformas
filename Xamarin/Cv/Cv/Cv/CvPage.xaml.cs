5636b3dc83e9336f292d69a64b24f8ec2c33e8fd
Paul Becerra <paulbecerracs@gmail.com>
2023-06-23 15:39:00 -0500

    added cv app

---
+++ Xamarin/Cv/Cv/Cv/CvPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cv
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CvPage : ContentPage
+    {
+        public CvPage(string nombre,string edad,string ocupa,string dire)
+        {
+            InitializeComponent();
+
+            Label nombrelabel=this.FindByName<Label>("nombre");
+            Label edadlabel = this.FindByName<Label>("edad");
+            Label ocupalabel = this.FindByName<Label>("ocupa");
+            Label direlabel = this.FindByName<Label>("dire");
+
+            nombrelabel.Text = nombre;
+            edadlabel.Text = edad;
+            ocupalabel.Text = ocupa;
+            direlabel.Text = dire;
+        }
+    }
+}
---
+++ Xamarin/Cv/Cv/Cv/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cv
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            string labelnombre = this.FindByName<Entry>("nombre").Text;
+            string labeledad = this.FindByName<Entry>("edad").Text;
+            string labelocupa = this.FindByName<Entry>("ocupa").Text;
+            string labeldire = this.FindByName<Entry>("dire").Text;
+
+            await Navigation.PushAsync(new CvPage(labelnombre,labeledad,labelocupa,labeldire));
+        }
+    }
+}
